a9d2bb3931f0fb78699221ae9dd7f3218098df5b
gregorio-estevez <gregoriomestevez12@gmail.com>
2023-09-09 00:37:27 -0400

    env9

--- database.py
+++ database.py
@@ -11,6 +11,7 @@ import os
 load_dotenv(override=False)
 
 uri = os.getenv('DATABASE_URL')
+print(uri)
 
 # Create a new client and connect to the server
 client = MongoClient(uri, server_api=ServerApi('1'))
