a774a6d9ef3b4d34533d56e8cd2d0f8f2f3f4582
gregorio-estevez <gregoriomestevez12@gmail.com>
2023-09-08 23:49:14 -0400

    agregando conexion a bd, junto mini prueba para IA y gitignore

---
+++ database.py
@@ -0,0 +1,30 @@
+from cgi import print_environ
+import collections
+from pydoc import cli
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+
+from dotenv import load_dotenv
+
+import os
+
+load_dotenv(override=False)
+
+uri = os.getenv('DATABASE_URL')
+
+# Create a new client and connect to the server
+client = MongoClient(uri, server_api=ServerApi('1'))
+
+# Send a ping to confirm a successful connection
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
+
+Database = client.get_database('proyect-tutorIA')
+
+CTGrammar = Database.get_collection('Grammar')
+CTText = Database.get_collection('Text')
+CTUser = Database.get_collection('User')
+CTVocabulary = Database.get_collection('Vocabulary')
