c9ba394ecaeb868381d58f825a4697f64d584355
gregorio-estevez <gregoriomestevez12@gmail.com>
2023-09-09 01:12:02 -0400

    env definitivo

--- database.py
+++ database.py
@@ -15,19 +15,18 @@ uri = os.getenv('DATABASE_URL')
 
 # Create a new client and connect to the server
 client = MongoClient(uri, server_api=ServerApi('1'))
-print(uri)
 
 # Send a ping to confirm a successful connection
 try:
     client.admin.command('ping')
     print("Pinged your deployment. You successfully connected to MongoDB!")
-    Database = client.get_database('proyect-tutorIA')
-    CTGrammar = Database.get_collection('Grammar')
-    CTText = Database.get_collection('Text')
-    CTUser = Database.get_collection('User')
-    CTVocabulary = Database.get_collection('Vocabulary')
 except Exception as e:
     print(e)
+    exit(1)
     
-    
+Database = client.get_database('proyect-tutorIA')
+CTGrammar = Database.get_collection('Grammar')
+CTText = Database.get_collection('Text')
+CTUser = Database.get_collection('User')
+CTVocabulary = Database.get_collection('Vocabulary')
 
