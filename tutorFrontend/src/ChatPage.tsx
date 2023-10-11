1e00aee0f83953e7d45f46a3b6a79018b742199b
Jean Azar <jeancarlosazar@gmail.com>
2023-10-11 08:24:22 -0400

    ChatComponents

---
+++ tutorFrontend/src/ChatPage.tsx
@@ -0,0 +1,13 @@
+// import "./ChatPage.css";
+import Chat from "./components/ChatBot";
+// import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+
+function ChatPage() {
+  return (
+    <div className="Chat">
+      <Chat />
+    </div>
+  );
+}
+
+export default ChatPage;
