7274106e5e86bba67ad52e09f85b35a4debdd440
Jean Azar <jeancarlosazar@gmail.com>
2023-10-11 08:27:11 -0400

    Rutas

--- tutorFrontend/src/App.tsx
+++ tutorFrontend/src/App.tsx
@@ -1,12 +1,24 @@
+import React from "react";
 import "./App.css";
 import LoginForm from "./components/LoginForm";
-// import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import ChatBot from "./components/ChatBot"; // Asegúrate de importar tu componente ChatBot
 
 function App() {
   return (
-    <div className="Login">
-      <LoginForm />
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<LoginForm />} />
+          <Route path="/ChatPage" element={<ChatBot />} />
+          {/* Asegúrate de tener un componente para ChatPage o reemplázalo con el componente que desees */}
+        </Routes>
+        <div className="Login">
+          <LoginForm />
+        </div>
+        <Link to="/ChatPage">Ir al Chat</Link>
+      </div>
+    </Router>
   );
 }
 
