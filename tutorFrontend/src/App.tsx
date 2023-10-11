72df26692ba4ca6d32f0806cdf02990f72065505
Jean Azar <jeancarlosazar@gmail.com>
2023-10-10 20:33:10 -0400

    routes

--- tutorFrontend/src/App.tsx
+++ tutorFrontend/src/App.tsx
@@ -1,6 +1,6 @@
 import "./App.css";
 import LoginForm from "./components/LoginForm";
-// import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 function App() {
   return (
