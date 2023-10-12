e5e373c10c65ec515e75a598b5b1200a25309ddb
Gregorio-Estevez <73475329+Gregorio-Estevez@users.noreply.github.com>
2023-10-12 18:10:39 -0400

    Update sample_test.py

--- tests/sample_test.py
+++ tests/sample_test.py
@@ -1,9 +1,20 @@
-from config.database import client
+import os
+from dotenv import load_dotenv
+from pymongo import MongoClient 
+from pymongo.server_api import ServerApi
+
+load_dotenv(override=False)
+
+uri = os.getenv('Database_URL')
+
+client = MongoClient(uri, server_api=ServerApi('1'))
 
 def test_connection_database():
   try:
-     client.admin.command('ping')
+     client.admin.command("ping")
      print("Pinged your deployment. You successfully connected to MongoDB!")
   except Exception as e:
      print(e)
      exit(1)
+
+test_connection_database()
